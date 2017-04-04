8955579a3004ffd05bf9d756b9830e50f52e4077
cashc <cashc@cash.me>
2017-04-03 23:21:29 -0400

    Added update functionality at links/<id> route

--- myAmbassador/models.py
+++ myAmbassador/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Link(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.TextField()
-    clicks = models.IntegerField()
+    clicks = models.IntegerField(default=0)
--- myAmbassador/views.py
+++ myAmbassador/views.py
@@ -1,8 +1,56 @@
+from django.http import HttpResponse, JsonResponse
 from .models import Link
 from rest_framework import viewsets
 from .serializers import LinkSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 
 class LinkViewSet(viewsets.ModelViewSet):
     queryset = Link.objects.all().order_by('id')
     serializer_class = LinkSerializer
+
+    @csrf_exempt
+    def link_list(request):
+        """
+        List all code links, or create a new link.
+        """
+        if request.method == 'GET':
+            links = Link.objects.all()
+            serializer = LinkSerializer(links, many=True)
+            return JsonResponse(serializer.data, safe=False)
+
+        elif request.method == 'POST':
+            data = JSONParser().parse(request)
+            serializer = LinkSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.errors, status=400)
+
+    @csrf_exempt
+    def link_detail(request, pk):
+        """
+        Retrieve, update or delete a code link.
+        """
+        try:
+            link = Link.objects.get(pk=pk)
+        except link.DoesNotExist:
+            return HttpResponse(status=404)
+
+        if request.method == 'GET':
+            serializer = LinkSerializer(link)
+            return JsonResponse(serializer.data)
+
+        elif request.method == 'PUT':
+            data = JSONParser().parse(request)
+            serializer = LinkSerializer(link, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors, status=400)
+
+        elif request.method == 'DELETE':
+            link.delete()
+            return HttpResponse(status=204)
